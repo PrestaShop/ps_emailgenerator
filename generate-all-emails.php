0ccc53a314deeb64d238251fa7dc7d480f9c31d1
Maxime Biloé <maxime.biloe@gmail.com>
2016-09-22 10:02:11 +0200

    CO: Do not display deprecated warnings

--- generate-all-emails.php
+++ generate-all-emails.php
@@ -1,7 +1,8 @@
 <?php
 
 include dirname(__FILE__).'/../../config/config.inc.php';
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 include dirname(__FILE__).'/ps_emailgenerator.php';
 
 $psEmailGenerator = new Ps_EmailGenerator();
-echo $psEmailGenerator->generateAllEmail();
+$psEmailGenerator->generateAllEmail();
